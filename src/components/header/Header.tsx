79d792411d7a9bcc732eb5e45692079c3fdd2906
erik ozzetti martins <erik.ozzetti.martins@gmail.com>
2022-04-27 00:01:37 -0300

    commponent header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,23 +1,23 @@
 import { Flex, useBreakpointValue } from "@chakra-ui/react";
-import React, { useContext  } from "react";
-import { UserContext } from "../../context/UserContext";
-import Button from "../forms/Buton";
+import React, { useContext } from "react";
+import { UserContext } from "context/UserContext";
+import { Button } from "../forms";
 import { useNavigate } from "react-router-dom";
 import { Logo } from "./Logo";
 import { Profile } from "./Profile";
 import { SearchBox } from "./SearchBox";
 
 function Header() {
-  const navigate = useNavigate()
-  const { login, userLogout, data , loading} = useContext(UserContext);
+  const navigate = useNavigate();
+  const { login, userLogout, data, loading } = useContext(UserContext);
   const isWideVersion = useBreakpointValue({
-    base:false ,
-    lg:true,
-  })
+    base: false,
+    lg: true,
+  });
 
   return (
     <>
-      { !loading && login && (
+      {!loading && login && (
         <Flex
           as="header"
           w="100%"
@@ -27,16 +27,21 @@ function Header() {
           align="center"
           bg="gray.700"
         >
-      <Logo />
-      {isWideVersion &&   <SearchBox /> } 
-      <Flex
-        align="center"
-        ml="auto"
-      >
-        <Button variant="outline"  onClick={()=> navigate('/task')} mr="18">Criar task</Button>
-        <Button variant="outline"  onClick={()=> userLogout()} mr="18">deslogar</Button>
-        <Profile  nome={data?.nome} email={data?.email}  showProfileData={isWideVersion} />
-      </Flex>
+          <Logo />
+          {isWideVersion && <SearchBox />}
+          <Flex align="center" ml="auto">
+            <Button variant="outline" onClick={() => navigate("/task")} mr="18">
+              Criar task
+            </Button>
+            <Button variant="outline" onClick={() => userLogout()} mr="18">
+              deslogar
+            </Button>
+            <Profile
+              nome={data?.nome}
+              email={data?.email}
+              showProfileData={isWideVersion}
+            />
+          </Flex>
         </Flex>
       )}
     </>
